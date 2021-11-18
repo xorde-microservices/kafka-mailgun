eaaa996692c0f28c2b2988dfc7684fd149b50e03
XT <xt@xorde.co>
2021-11-17 19:47:11 -0500

    Updated CI

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -14,8 +14,14 @@ if [ "${1}" = "down" ]; then
   exit
 fi
 
+### run detached if argument is "detach"
+if [ "${1}" = "detach" ]; then
+  echo "Will run stack detached:"
+  OPT=-d
+fi
+
 echo "Building new docker image:"
 docker-compose build
 
 echo "Deploying/redeploying services:"
-docker-compose --env-file .env up --force-recreate --remove-orphans
+docker-compose --env-file .env up ${OPT} --force-recreate --remove-orphans
